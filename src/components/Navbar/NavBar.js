3c7007f4558275a20609f05b17a67ec8cbe215e2
matias perona <mlperona@yahoo.com.ar>
2022-02-07 18:22:55 -0300

    Creacion de NavBar

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () =>{
+    return(
+        <nav>
+            <p>
+                Mi tienda
+            </p>
+            <button>Todos</button>
+            <button>Cervezas</button>
+            <button>Licores</button>
+            <button>Whisky</button>
+        </nav>
+    )
+}
+export default NavBar
